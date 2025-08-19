f7a7174e4da724efa2e1f6888a86ebea660a7a0c
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-08-19 05:56:08 +0530

    add icon and logo to Home page

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <nav
-      className="nav w-screen p-4 flex items-center justify-between bg-slate-50
+      className="nav w-screen h-[6.5rem] p-1 flex items-center justify-between bg-white
 "
     >
       <div className="flex justify-between">
-        <h1 className="text-4xl">Tutor-Tube</h1>
+        <img className="ml-5 h-[15rem]" src="./logo2.png" alt="logo" />
         <button className="menu-btn hidden">
           <img className="w-auto h-10" src="./menu.svg" alt="menu-icon" />
         </button>
       </div>
-      <div className="menu-links flex gap-8 text-2xl p-3">
+      <div className="menu-links flex gap-8 text-2xl p-1 mr-5">
         <Link to="/get-started">Get Started</Link>
         <Link to="/courses">Courses</Link>
         <Link to="/dashboard">Dashboard</Link>
