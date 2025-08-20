007251d6b242d90ef64312c7caad02dea41baae0
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-08-20 07:03:29 +0530

    add Courses page to routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
+import Courses from "./views/Courses";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/courses" element={<Courses />} />
     </Routes>
   );
 }
---
+++ frontend/src/views/Courses.jsx
@@ -0,0 +1,11 @@
+import Nav from "../components/Nav";
+
+function Courses() {
+  return (
+    <div className="flex flex-col">
+      <Nav />
+    </div>
+  );
+}
+
+export default Courses;
