c4a4ffbea28b269a180e568d761eb45996107df8
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-08-22 20:48:38 +0530

    make Footer responsive

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="py-3.5 w-screen text-center bg-[#1a1a1a] text-white">
+    <footer className="w-screen text-center py-[0.6rem] text-[0.8rem] md:py-[0.5rem] md:text-[0.9rem] bg-[#1a1a1a] text-white">
       <p>&copy; {new Date().getFullYear()} TutorTube. All rights reserved.</p>
     </footer>
   );
