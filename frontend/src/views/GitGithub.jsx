e063acca07e8673255af2f745b4bc8e597b08760
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-08-24 15:46:44 +0530

    add CoursePageParagraph component

--- frontend/src/views/GitGithub.jsx
+++ frontend/src/views/GitGithub.jsx
@@ -1,6 +1,7 @@
 import Course from "../components/course/Course";
 import CoursePage from "../components/course/CoursePage";
 import CoursePageHeading from "../components/course/CoursePageHeading";
+import CoursePageParagraph from "../components/course/CoursePageParagraph";
 import CoursePageVideo from "../components/course/CoursePageVideo";
 function GitGithub() {
   return (
@@ -14,6 +15,19 @@ function GitGithub() {
           source="https://www.youtube.com/embed/Yc8sCSeMhi4"
           videoTitle="What Is Version Control? | Git Version Control | Version Control In Software Engineering|Simplilearn"
         />
+        <CoursePageParagraph>
+          The first module establishes the fundamentals of version control—the
+          practice of tracking and managing changes to code over time. This
+          process is enabled by Version Control Systems (VCS), which document
+          project history and ensure efficient collaboration.
+          <br />
+          <br />
+          Without version control, teams resort to saving multiple file versions
+          (e.g., version1, version2), leading to confusion, conflicting changes,
+          and no reliable way to revert to a stable state. In contrast, a VCS
+          provides a structured, branched history that supports collaboration,
+          accountability, and recovery.
+        </CoursePageParagraph>
       </CoursePage>
 
       <CoursePage
